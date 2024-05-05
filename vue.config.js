a8587d067cfcc789e0957de1ffde1a8e5f65dbc2
ruot ogawa <ryotoogawa1999@gmail.com>
2024-05-05 20:23:47 +0900

    aaa

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: './' //npm run build で生成された　distディレクトリのindex.jsのパス指定の調整
+  //参考：https://qiita.com/kohecchi/items/5d06936745118045a308
 })
